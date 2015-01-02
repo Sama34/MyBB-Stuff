b012bf93229ffeb291ba68591b3291069827ab27
Zalvie <Zalvie@users.noreply.github.com>
2015-01-02 12:07:20 +0100

    Update temp_email_blocker.php

--- temp_email_blocker.php
+++ temp_email_blocker.php
@@ -9,7 +9,7 @@
 	$emails = fetch_remote_file('https://raw.githubusercontent.com/martenson/disposable-email-domains/master/disposable_email_blacklist.conf');
 
 	foreach (array_filter(array_map($func, explode(PHP_EOL, $emails))) as $e)
-		$inserts[] = ['filter' => $db->escape_string(trim($e)), 'type' => 3, 'dateline' => TIME_NOW];
+		$inserts[] = ['filter' => $db->escape_string('*@' . trim($e)), 'type' => 3, 'dateline' => TIME_NOW];
 
 	$db->insert_query_multiple("banfilters", $inserts);
 
